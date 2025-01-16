1d304483676f9ed5f1afd016cdd26cf14f705b07
thanhan34 <dtan42@gmail.com>
2025-01-16 10:17:56 +0700

    refactor: add getAdminStats function export for admin statistics retrieval

--- app/firebase/services/index.ts
+++ app/firebase/services/index.ts
@@ -7,6 +7,7 @@ export * from './class';
 export { getHomeworkSubmissions, saveHomeworkSubmission } from './homework';
 export * from './notification';
 export { getHomeworkProgress, getWeeklyProgress, getDailyProgress } from './progress';
+export { getAdminStats } from './admin';
 
 // Re-export specific functions for backward compatibility
 export { getUserByEmail, createUser } from './user';
